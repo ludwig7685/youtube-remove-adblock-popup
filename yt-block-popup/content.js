b36c4d92a26ef466e1117751750e26afa0adeac4
ludwig7685 <139857057+ludwig7685@users.noreply.github.com>
2023-10-19 14:34:26 +0200

    Update content.js

--- yt-block-popup/content.js
+++ yt-block-popup/content.js
@@ -2,11 +2,38 @@
 
 const elementSelector = 'tp-yt-paper-dialog.style-scope.ytd-popup-container';
 
+let hasStarted = 0; // Variable to track the number of times the video has started
+let timer; // Timer to prevent constant checking
+
+// Function to start the video
+function startVideo() {
+  const videoElement = document.querySelector('video'); // Select the video element
+
+  if (videoElement && videoElement.paused) {
+    if (videoElement.currentTime <= 20 && hasStarted < 1) {
+      videoElement.play();
+      hasStarted++;
+    }
+    // If the video duration is above 20 seconds, add a delay to check again
+    else if (videoElement.duration > 20) {
+      clearTimeout(timer); // Clear the existing timer
+      // Set a new timer to check every 10 minutes
+      timer = setTimeout(checkVideo, 10 * 60 * 1000); // 10 minutes in milliseconds
+    }
+  }
+}
+
+// Function to remove the popup element
 function removeElement() {
   const element = document.querySelector(elementSelector);
   if (element) {
     element.remove();
   }
+}
+
+// Timer function to prevent constant checking
+function checkVideo() {
+  startVideo();
 }
 
 // Observe changes in the DOM
@@ -18,7 +45,12 @@ const observer = new MutationObserver((mutationsList) => {
   }
 });
 
-observer.observe(document.documentElement, { childList: true, subtree: true });
+// Listen for changes in the video state (e.g., pause and play)
+document.querySelector('video').addEventListener('pause', startVideo);
 
-// Call the removeElement function initially
-removeElement();
+// Add an event listener for page load to start the video and observe the DOM
+document.addEventListener('DOMContentLoaded', () => {
+  startVideo();
+  observer.observe(document.documentElement, { childList: true, subtree: true });
+  removeElement(); // Call the removeElement function initially
+});
